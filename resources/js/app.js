8993f0dd8e4d7fd9dffef744b932df1f7a884428
MHchongg <mingbh411@gmail.com>
2024-11-15 14:45:46 +0800

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,24 @@
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/vue3'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import Toast, { POSITION } from "vue-toastification"
+import 'vue-toastification/dist/index.css'
+
+createInertiaApp({
+    resolve: name => {
+        const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
+        return pages[`./Pages/${name}.vue`]
+    },
+    setup({ el, App, props, plugin }) {
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ElementPlus)
+            .use(Toast, {
+                position: POSITION.TOP_CENTER,
+                timeout: 2500,
+                newestOnTop: true,
+            })
+            .mount(el)
+    },
+})
